a78be29b35071b156a706e0d62cf219ef853e437
Milad Alizadeh <milad.alizadeh@breakthroughmedia.org>
2018-01-25 09:53:11 +0000

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
                 test: /\.scss$/,
                 loader: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    loader: [
+                    use: [
                         'css-loader',
                         {
                             loader: 'postcss-loader',
