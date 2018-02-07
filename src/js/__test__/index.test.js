10cdc2c21c80964abbcdf720fb17271c3f79e460
Milad Alizadeh <milad.alizadeh@breakthroughmedia.org>
2018-02-07 17:21:38 +0000

    remove redundant test

--- src/js/__test__/index.test.js
+++ src/js/__test__/index.test.js
@@ -1,10 +1,6 @@
 import LazyLoader from '../index';
 
 describe('Root', () => {
-    test('constructor()', () => {
-        // let lazyLoader = new LazyLoader();
-    });
-
     test('getDataNodes()', () => {
         document.body.innerHTML = `
             <div class="container-1">
@@ -21,30 +17,5 @@ describe('Root', () => {
 
         expect(dataNodes.length).toBe(2);
         expect(dataNodes[0].classList.contains('image-1')).toBe(true);
-    });
-
-    test('onImageCreated()', () => {
-        // // Input div with child image
-        // let inputNode = document.createElement('div');
-        // let childNode = new Image();
-        // childNode.setAttribute('id', 'original');
-        // inputNode.appendChild(childNode);
-        //
-        // // Output node with image of an id of 'new'
-        // let outputNode = document.createElement('div');
-        // let outputImage = new Image();
-        // outputImage.setAttribute('id', 'new');
-        // outputNode.appendChild(outputImage);
-        //
-        // // An imitation of our 'outputImage' above
-        // let newImage = new Image();
-        // newImage.setAttribute('id', 'new');
-        //
-        // // Replace the childNode with our newImage node
-        // // This should then equal our outputNode
-        // let root = new LazyLoader();
-        // root.onImageCreated(childNode, newImage);
-        //
-        // expect(inputNode).toEqual(outputNode);
     });
 });
