ab1c282f9d2745f1597d4f2934e970e60e3927e9
Milad Alizadeh <milad.alizadeh@breakthroughmedia.org>
2018-02-07 16:06:07 +0000

    remove redundant test

--- src/js/__test__/imagenode.test.js
+++ src/js/__test__/imagenode.test.js
@@ -1,20 +1,6 @@
 import imagenode from '../imagenode';
-import options from '../options';
 
 describe('Image Node', () => {
-    let originalImage = (hasParams = true) => {
-        let img = new Image();
-        img.setAttribute('src', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=');
-        img.setAttribute('data-src', 'https://images.unsplash.com/photo-1485609315582-cfffa02888e8');
-        img.setAttribute('data-srcset', 'https://images.unsplash.com/photo-1485609315582-cfffa02888e8?fit=crop&w=200&q=80 200w');
-        img.setAttribute('data-sizes', '(max-width 200w)');
-        if (hasParams) {
-            img.setAttribute('alt', 'My Test Image');
-            img.setAttribute('class', 'testimage');
-        }
-        return img;
-    };
-
     test('getAttr() get class attribute successfully correctly', () => {
         let inputImage = new Image();
         inputImage.setAttribute('src', '#');
@@ -31,85 +17,6 @@ describe('Image Node', () => {
         let inputAttr = 'class';
         let output = null;
         expect(imagenode.getAttr(inputImage, inputAttr)).toBe(output);
-    });
-
-    test('setCustomAttrs() set custom attributes', () => {
-        let newImage = new Image();
-        imagenode.setCustomAttrs(originalImage(), newImage, options.setOptions());
-
-        let output = new Image();
-        output.setAttribute('src', 'https://images.unsplash.com/photo-1485609315582-cfffa02888e8');
-        output.setAttribute('srcset', 'https://images.unsplash.com/photo-1485609315582-cfffa02888e8?fit=crop&w=200&q=80 200w');
-        output.setAttribute('sizes', '(max-width 200w)');
-
-        expect(newImage).toEqual(output);
-    });
-
-    test('setCustomAttrs() set custom attributes should add no attributes if none are set', () => {
-        let previousImage = new Image();
-        let newImage = new Image();
-        imagenode.setCustomAttrs(previousImage, newImage, options.setOptions());
-        expect(newImage).toEqual(new Image());
-    });
-
-    test('setAdditionalAttrs() set additional attributes', () => {
-        let newImage = new Image();
-        imagenode.setAdditionalAttrs(originalImage(), newImage, options.setOptions());
-
-        let output = new Image();
-        output.setAttribute('alt', 'My Test Image');
-        output.setAttribute('class', 'testimage');
-
-        expect(newImage).toEqual(output);
-    });
-
-    test('setAdditionalAttrs() set additional attributes with no attributes', () => {
-        let newImage = new Image();
-        imagenode.setAdditionalAttrs(originalImage(false), newImage, options.setOptions());
-        expect(newImage).toEqual(new Image());
-    });
-
-    test('getCustomImgAttrs() get custom attributes', () => {
-        let input = {
-            srcset: 'srcset',
-            src: 'src',
-            sizes: '400w'
-        };
-
-        let output = [
-            {
-                name: 'srcset',
-                value: 'srcset'
-            },
-            {
-                name: 'src',
-                value: 'src'
-            },
-            {
-                name: 'sizes',
-                value: '400w'
-            }
-        ];
-        expect(imagenode.getCustomImgAttrs(input)).toEqual(output);
-    });
-
-    test('getCustomImgAttrs() get custom attributes with undefined option', () => {
-        let input = {
-            srcset: 'srcset',
-            sizes: '400w'
-        };
-
-        let output = [
-            {
-                name: 'srcset',
-                value: 'srcset'
-            },
-            {
-                name: 'sizes',
-                value: '400w'
-            }
-        ];
-        expect(imagenode.getCustomImgAttrs(input)).toEqual(output);
     });
 
     test('setAttr() set attribute with value', () => {
