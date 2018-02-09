e61f3622e03eeb4fca99f94b5458c075fa50a74b
Milad Alizadeh <milad.alizadeh@breakthroughmedia.org>
2018-02-09 11:49:09 +0000

    refactor tests

--- src/js/__test__/options.test.js
+++ src/js/__test__/options.test.js
@@ -1,39 +1,41 @@
 import options from '../options';
 
 describe('Options', () => {
-    test('setOptions() new options to be combined with defaults', () => {
-        let input = {
-            throttle: 3000,
-            loadOnScroll: false,
-            src: 'data-core'
-        };
+    describe('setOptions()', () => {
+        test('it should combine the new options with defaults', () => {
+            let input = {
+                throttle: 3000,
+                loadOnScroll: false,
+                src: 'data-core'
+            };
 
-        let output = {
-            threshold: 0,
-            throttle: 3000,
-            resizeDebounce: 500,
-            loadOnScroll: false,
-            callback: undefined,
-            src: 'data-core',
-            srcset: 'data-srcset',
-            sizes: 'data-sizes'
-        };
-        expect(options.setOptions(input)).toEqual(output);
-    });
+            let output = {
+                threshold: 0,
+                throttle: 3000,
+                resizeDebounce: 500,
+                loadOnScroll: false,
+                callback: undefined,
+                src: 'data-core',
+                srcset: 'data-srcset',
+                sizes: 'data-sizes'
+            };
+            expect(options.setOptions(input)).toEqual(output);
+        });
 
-    test('setOptions() with a bad input should return default parameters', () => {
-        let input = 'a string';
+        test('it should return default parameters if a bad input is set', () => {
+            let input = 'a string';
 
-        let output = {
-            threshold: 0,
-            throttle: 250,
-            resizeDebounce: 500,
-            loadOnScroll: true,
-            callback: undefined,
-            src: 'data-src',
-            srcset: 'data-srcset',
-            sizes: 'data-sizes'
-        };
-        expect(options.setOptions(input)).toEqual(output);
+            let output = {
+                threshold: 0,
+                throttle: 250,
+                resizeDebounce: 500,
+                loadOnScroll: true,
+                callback: undefined,
+                src: 'data-src',
+                srcset: 'data-srcset',
+                sizes: 'data-sizes'
+            };
+            expect(options.setOptions(input)).toEqual(output);
+        });
     });
 });
